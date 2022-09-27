2a33e4747755333a3c2afae9395559e53b37c375
Ninguem2k <cassioriachinho@gmai.com>
2022-09-27 15:46:19 -0300

    SSpring Boot Essentials 02 - @Component, @Autowired e @SpringBootApplication - Video 03

--- src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -2,10 +2,13 @@ package br.com.devdojo.awesome.endpoint;
 
 
 import br.com.devdojo.awesome.model.Student;
+import br.com.devdojo.awesome.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -14,8 +17,12 @@ import static java.util.Arrays.asList;
 @RestController
 @RequestMapping("student")
 public class StudentEndpoint {
+    @Autowired
+    private DateUtil dateUtil;
     @RequestMapping(method = RequestMethod.GET,path = "/list")
     public List<Student> listAll(){
+        System.out.println(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+
         return asList(new Student("deku"), new Student("cássio"));
     }
 }
